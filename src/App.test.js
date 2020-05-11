b687312581a99ab3c5e88642c16078d3cc25daa9
Tim Harsch <tim@unitegenomics.com>
2020-05-11 13:38:18 -0700

    Switch to using enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
+/*import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/enzyme-grr/i);
   expect(linkElement).toBeInTheDocument();
+});
+*/
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+it('renders without crashing', () => {
+  shallow(<App />);
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+//import '@testing-library/jest-dom/extend-expect';
+
+// SETUP Enzyme per create-react-app docs https://create-react-app.dev/docs/running-tests#srcsetuptestsjs
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
